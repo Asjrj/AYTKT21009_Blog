164e21cc3245228df73ff782a2cc1d33060021d5
Antti Penttinen <asjrj200@gmail.com>
2018-10-18 11:24:31 +0300

    Tehtävä 4.4

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -1,8 +1,26 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy is called', () => {
-  const blogs = []
+describe('DUMMY', () => {
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+  test('dummy is called', () => {
+    const blogs = []
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+  })
+
+})
+
+describe('Total likes', () => {
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listHelper.dummyBlogs)
+    expect(result).toBe(36)
+  })
+
+
 })
