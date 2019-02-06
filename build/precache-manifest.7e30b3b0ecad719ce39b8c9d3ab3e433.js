7fe29c1c1dcc5f587dcf4d411fcbc44afd33a9e2
Antti Penttinen <asjrj200@gmail.com>
2019-02-06 10:07:58 +0200

    Security fix

---
+++ build/precache-manifest.7e30b3b0ecad719ce39b8c9d3ab3e433.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "16cd134d3c1cdb263107",
+    "url": "/static/js/main.16cd134d.chunk.js"
+  },
+  {
+    "revision": "f65e0c9997e34650e407",
+    "url": "/static/js/1.f65e0c99.chunk.js"
+  },
+  {
+    "revision": "16cd134d3c1cdb263107",
+    "url": "/static/css/main.cbfd3c5b.chunk.css"
+  },
+  {
+    "revision": "f6e915d5301daf2a41c7375754ff9f12",
+    "url": "/index.html"
+  }
+];
